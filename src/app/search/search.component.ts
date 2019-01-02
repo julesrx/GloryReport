8c2108e66b7d4d562d7a290edc2ce2679782f4d2
JulesRx <25800503+JulesRx@users.noreply.github.com>
2019-01-02 09:17:00 +0100

    Instant redirection if only one character is found

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -39,6 +39,11 @@ export class SearchComponent implements OnInit, OnDestroy {
       ((res: ServerResponse<UserInfoCard[]>) => {
         this.players = res.Response;
         console.log(this.players)
+
+        if (this.players.length == 1) {
+          var player = this.players[0];
+          this.router.navigate(['/report', player.membershipType, player.membershipId]);
+        }
       })
     );
 
