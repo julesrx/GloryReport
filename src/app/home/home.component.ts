b828993bafcc634831d720162f3c656ba3cb753d
JulesRx <25800503+JulesRx@users.noreply.github.com>
2019-01-03 13:44:19 +0100

    New search home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
-import { BungieMembershipType } from 'bungie-api-ts/common';
+import { BungieMembershipType, ServerResponse } from 'bungie-api-ts/common';
+import { UserInfoCard } from 'bungie-api-ts/user';
+import { Observable, Subscription, forkJoin } from 'rxjs';
+import { BungieHttpService } from '../services/bungie-http.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  public searching: boolean;
+  public displayResults: boolean;
+  public errors: string[];
+
+  public player: UserInfoCard[];
+  public opponent: UserInfoCard[];
+
+  private playerObs: Observable<ServerResponse<UserInfoCard[]>>;
+  private opponentObs: Observable<ServerResponse<UserInfoCard[]>>;
+
+  private searchResponse: Subscription;
 
   public searchForm: FormGroup;
   public membershipTypes: any[];
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private bHttp: BungieHttpService,
   ) { }
 
   ngOnInit() {
+    this.errors = [];
+
     this.initForm();
 
     this.membershipTypes = [
@@ -31,17 +50,77 @@ export class HomeComponent implements OnInit {
 
   initForm() {
     this.searchForm = this.formBuilder.group({
-      membershipType: [Validators.required],
-      name: ['', Validators.required]
+      membershipType: [BungieMembershipType.TigerXbox, Validators.required],
+      player: ['Myjulot', Validators.required],
+      opponent: ['NeqZaah', Validators.required]
     })
   }
 
   search() {
-    const membershipType = this.searchForm.get('membershipType').value;
-    const name = this.searchForm.get('name').value;
+    this.errors = [];
 
-    if (name.length) {
-      this.router.navigate(['/search', membershipType, name]);
-    }
+    const membershipType = this.searchForm.get('membershipType').value;
+    const playerName = this.searchForm.get('player').value;
+    const opponentName = this.searchForm.get('opponent').value;
+
+    this.searching = true;
+
+    this.playerObs = this.bHttp.get(
+      'https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/' + encodeURIComponent(membershipType) + '/' + encodeURIComponent(playerName) + '/'
+    );
+    this.opponentObs = this.bHttp.get(
+      'https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/' + encodeURIComponent(membershipType) + '/' + encodeURIComponent(opponentName) + '/'
+    );
+
+    this.searchResponse = forkJoin([this.playerObs, this.opponentObs]).subscribe(
+      (results) => {
+        this.player = results[0].Response;
+        this.opponent = results[1].Response;
+
+        console.log(this.player)
+        console.log(this.opponent)
+
+        if (!this.player.length) {
+          this.errors.push('The player ' + playerName + ' was not found');
+        }
+        if (!this.opponent.length) {
+          this.errors.push('The player ' + opponentName + ' was not found');
+        }
+
+        if (this.player.length && this.opponent.length) {
+          console.log('ALL OK');
+        }
+      },
+      (error) => {
+        console.log('ERROR')
+      },
+      () => {
+        this.displayResults = true;
+      }
+    );
+
+    // this.bHttp.get(
+    //   'https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/' + encodeURIComponent(membershipType) + '/' + encodeURIComponent(playerName) + '/'
+    // ).subscribe(
+    //   ((res: ServerResponse<UserInfoCard[]>) => {
+    //     this.player = res.Response;
+    //     console.log(this.player)
+    //   })
+    // );
+
+    // this.bHttp.get(
+    //   'https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/' + encodeURIComponent(membershipType) + '/' + encodeURIComponent(opponentName) + '/'
+    // ).subscribe(
+    //   ((res: ServerResponse<UserInfoCard[]>) => {
+    //     this.opponent = res.Response;
+    //     console.log(this.opponent)
+    //   })
+    // );
+
+    this.searching = false;
+  }
+
+  ngOnDestroy() {
+    this.searchResponse.unsubscribe();
   }
 }
