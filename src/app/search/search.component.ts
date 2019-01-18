afa4f28cb5baf3c27d4a5b34b6e6b6e5cace9788
JulesRx <25800503+JulesRx@users.noreply.github.com>
2019-01-18 10:01:14 +0100

    Working example

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
-import { Subscription, Observable, forkJoin, BehaviorSubject } from 'rxjs';
+import { Subscription, BehaviorSubject, empty as observableEmpty, throwError as observableThrowError } from 'rxjs';
+import { catchError, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 
-import { ServerResponse, UserInfoCard, BungieMembershipType } from 'bungie-api-ts/user';
+import { ServerResponse, UserInfoCard } from 'bungie-api-ts/user';
 
 import { BungieHttpService } from '../services/bungie-http.service';
-import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -17,67 +17,67 @@ export class SearchComponent implements OnInit, OnDestroy {
   public searching: boolean;
 
   public player: UserInfoCard;
-  public opponent: UserInfoCard;
 
-  private membershipTypeR: BehaviorSubject<string>;
+  private membershipType: BehaviorSubject<string>;
   private playerName: BehaviorSubject<string>;
-  private opponentName: BehaviorSubject<string>;
-
-  private playerObs: Observable<ServerResponse<UserInfoCard>>;
-  private opponentObs: Observable<ServerResponse<UserInfoCard>>;
 
   private searchResponse: Subscription;
   private params$: Subscription;
 
   constructor(
-    private router: Router,
     private route: ActivatedRoute,
     private bHttp: BungieHttpService,
   ) { }
 
   ngOnInit() {
-    this.membershipTypeR = new BehaviorSubject('');
+    // this.membershipType = new BehaviorSubject('');
     this.playerName = new BehaviorSubject('');
-    this.opponentName = new BehaviorSubject('');
 
     this.searching = true;
 
     this.params$ = this.route.params.subscribe((params: Params) => {
-      if (params['membershipType'] && params['player'] && params['opponent']) {
-        this.membershipTypeR.next(params['membershipType']);
+      if (params['player']) {
+        // this.membershipType.next(params['membershipType']);
         this.playerName.next(params['player']);
-        this.opponentName.next(params['opponent']);
-
-        console.log(params);
       }
     });
 
-    const membershipType = +this.route.snapshot.paramMap.get('membershipType');
-    const playerName = this.route.snapshot.paramMap.get('player');
-    const opponentName = this.route.snapshot.paramMap.get('opponent');
+    // this.searchResponse = this.membershipType
+    //   .subscribe(res => {
+    //     console.log(res);
+    //   });
 
-    this.playerObs = this.bHttp.get(
-      'https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/' + membershipType + '/' + encodeURIComponent(playerName) + '/'
-    );
-    this.opponentObs = this.bHttp.get(
-      'https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/' + membershipType + '/' + encodeURIComponent(opponentName) + '/'
-    );
+    this.searchResponse = this.playerName
+      .pipe(
+        map(playerName => {
+          this.searching = true;
+          if (playerName.length) {
+            return 'https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/1/' + encodeURIComponent(playerName) + '/';
+          } else {
+            return '';
+          }
+        }),
+        distinctUntilChanged(),
+        switchMap(url => {
+          if (url.length) {
+            return this.bHttp
+              .get(url)
+              .pipe(
+                catchError((error: any) =>
+                  observableThrowError(error.json().error || 'Server error')
+                )
+              );
+          } else {
+            return observableEmpty();
+          }
+        })
+      )
+      .subscribe((res: ServerResponse<UserInfoCard[]>) => {
+        this.player = res.Response[0];
+        console.log(this.player);
 
-    this.searchResponse = forkJoin([this.playerObs, this.opponentObs]).subscribe(
-      (results) => {
-        this.player = results[0].Response;
-        this.opponent = results[1].Response;
-
-        console.log(this.player)
-        console.log(this.opponent)
-
-        // this.router.navigate(['/report', this.player.membershipId, this.opponent.membershipId]);
-      },
-      (error) => {
-        console.log(error)
-      },
-      () => { }
-    );
+        this.searching = false;
+      });
 
     this.searching = false;
   }
