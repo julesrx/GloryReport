a7588f79d1c7dc57f01f83607f78a26b3de5123a
JulesRx <25800503+JulesRx@users.noreply.github.com>
2019-01-30 09:19:09 +0100

    Global alerts

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
+import { BungieHttpService } from '../services/bungie-http.service';
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -12,14 +14,23 @@ export class NavComponent implements OnInit {
   public title: string;
   public searchName: string;
 
+  public globalAlerts: [];
+
   constructor(
     private router: Router,
-    private titleService: Title
+    private titleService: Title,
+    private bHttp: BungieHttpService,
   ) { }
 
   ngOnInit() {
     this.title = this.titleService.getTitle();
     this.searchName = '';
+
+    this.bHttp
+      .get('https://www.bungie.net/Platform/GlobalAlerts/')
+      .subscribe((res) => {
+        this.globalAlerts = res.Response;
+      });
   }
 
   search() {
