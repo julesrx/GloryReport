f4bd95939f8a35f3c9bec4951b13c1528053cae0
JulesRx <25800503+JulesRx@users.noreply.github.com>
2019-04-17 09:21:56 +0200

    Cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,22 @@
-import { BrowserModule, Title } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { MarkdownModule, MarkedOptions } from 'ngx-markdown';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BungieHttpService } from './services/bungie-http.service';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { RoutesModule } from './routes/routes.module';
-import { SearchComponent } from './search/search.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavComponent } from './nav/nav.component';
-import { ReportComponent } from './report/report.component';
-import { FaqComponent } from './faq/faq.component';
-import { ChangelogComponent } from './changelog/changelog.component';
-import { LoadingReducePipe } from './pipes/loading-reduce.pipe';
-import { PlayerComponent } from './report/player/player.component';
-import { DetailedComponent } from './report/detailed/detailed.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchComponent,
-    HomeComponent,
-    FooterComponent,
-    NavComponent,
-    ReportComponent,
-    FaqComponent,
-    ChangelogComponent,
-    LoadingReducePipe,
-    PlayerComponent,
-    DetailedComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    RoutesModule,
-    MarkdownModule.forRoot({
-      loader: HttpClient,
-      markedOptions: {
-        provide: MarkedOptions
-      }
-    }),
+    // RoutesModule
   ],
   providers: [
     BungieHttpService
--- src/app/models/occurence.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Occurence } from './occurence';
-
-describe('Occurence', () => {
-  it('should create an instance', () => {
-    expect(new Occurence('0123456789', 'Name')).toBeTruthy();
-  });
-});
