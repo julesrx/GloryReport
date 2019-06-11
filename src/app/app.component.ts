08e5c5ed636206d5185ed8ea0de2ad0d5ca09c8c
JulesRx <25800503+JulesRx@users.noreply.github.com>
2019-06-11 10:17:01 +0200

    0.5.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  public title: string = 'GloryReport';
+  public constructor(private titleService: Title) { }
+
+  ngOnInit() {
+    // TODO: Add custom title servive to get version
+    this.titleService.setTitle('GloryReport 0.5.0');
+  }
 
 }
