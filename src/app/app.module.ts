6391e62764aad075e28c484c55dc28268e2083d7
julesrx <me@julesrx.fr>
2020-04-13 15:39:12 +0200

    card move

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ReportComponent } from './components/report/report.component';
 import { ManifestService } from './services/manifest.service';
 import { DestinyItemHashPipe } from './pipes/destiny-item-hash.pipe';
-import { CharacterCardComponent } from './components/ui/character-card/character-card.component';
+import { CharacterCardComponent } from './components/character-card/character-card.component';
 
 @NgModule({
   declarations: [
