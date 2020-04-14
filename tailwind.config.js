4512a278aea34569cff98de1bfb0878037b54f4d
julesrx <me@julesrx.fr>
2020-04-15 00:03:17 +0200

    brand color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   theme: {
     fontFamily: {
       'sans': ['Inter', 'system-ui', '-apple-system', '"Segoe UI"', 'Roboto', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        brand: '#c51611'
+      }
     }
   },
   variants: {},
