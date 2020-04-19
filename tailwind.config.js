fcaeed922fb62f46d101920ce1054fc80980ae4b
julesrx <me@julesrx.fr>
2020-04-19 23:16:24 +0200

    colors initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     },
     extend: {
       colors: {
-        main: '#c51611'
+        main: '#c51611',
+        gray: { // replaced the default cool grays with a neutral gray palette
+          '100': '#f5f5f5',
+          '200': '#eeeeee',
+          '300': '#e0e0e0',
+          '400': '#bdbdbd',
+          '500': '#9e9e9e',
+          '600': '#757575',
+          '700': '#616161',
+          '800': '#424242',
+          '900': '#212121',
+        }
       },
       spacing: {
         'char': '96px',
