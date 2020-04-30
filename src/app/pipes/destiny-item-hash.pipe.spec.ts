099a32bd4efaf42954e51c0415465e130fc8fd96
julesrx <me@julesrx.fr>
2020-04-30 14:12:20 +0200

    skipTests

--- src/app/pipes/destiny-item-hash.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DestinyItemHashPipe } from './destiny-item-hash.pipe';
-
-describe('DestinyItemHashPipe', () => {
-  it('create an instance', () => {
-    // const pipe = new DestinyItemHashPipe();
-    // expect(pipe).toBeTruthy();
-  });
-});
