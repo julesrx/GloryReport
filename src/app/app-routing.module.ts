ffb5db6dd7ff3edba4e8ba3b8de27dc1574c53bd
julesrx <me@julesrx.fr>
2020-05-03 11:30:28 +0200

    Nested routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { ReportComponent } from './components/report/report.component';
 import { HomeComponent } from './components/home/home.component';
 import { EncountersComponent } from './components/encounters/encounters.component';
 
-const baseProfileRoute = ':membershipType/:membershipId/';
-
 const routes: Routes = [
-  { path: `${baseProfileRoute}encounters`, component: EncountersComponent },
-  { path: `${baseProfileRoute}report`, component: ReportComponent },
+  {
+    path: ':membershipType/:membershipId', children: [
+      { path: `encounters`, component: EncountersComponent },
+      { path: `report`, component: ReportComponent },
+    ]
+  },
   { path: '', component: HomeComponent },
   { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
