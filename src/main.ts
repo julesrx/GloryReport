d184689fdbebefcce75275661a71d9e696b8ea59
julesrx <me@julesrx.fr>
2020-09-11 23:20:37 +0200

    env and packages

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import axios from 'axios';
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = true;
+
+Vue.prototype.$http = axios;
+Vue.prototype.$bhttp = axios.create({
+  baseURL: 'https://stats.bungie.net/Platform/',
+  headers: { 'X-Api-Key': process.env.VUE_APP_BUNGIE_API_KEY }
+});
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
