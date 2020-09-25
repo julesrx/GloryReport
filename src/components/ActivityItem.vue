8ea63ce0957c3ed2d3b3f6d1257a7d60dc7f8e76
julesrx <me@julesrx.fr>
2020-09-25 19:02:07 +0200

    typo

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -1,6 +1,6 @@
 <template>
   <a :href="dtr" target="_blank" class="activity-item flex items-center py-2 hover:bg-dark-600">
-    <p v-if="loading">Loading activity...</p>
+    <p v-if="loading">Loading details...</p>
     <template v-if="!loading && pgcr">
       <div :class="['h-6 w-1 mr-2', win ? 'bg-green-600' : null]"></div>
       <DateDistance :date="pgcr.period" />
