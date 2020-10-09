767dae70d7388eb93ca326ead2463ef2c1f774ee
julesrx <me@julesrx.fr>
2020-10-09 22:50:30 +0200

    Switch to router legacy mode

---
+++ src/router.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
+
+import Home from './views/Home.vue';
+import PlayerReport from './views/PlayerReport.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    name: 'Home',
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'PlayerReport',
+    path: '/:membershipType/:membershipId',
+    component: PlayerReport
+  },
+  {
+    name: 'NotFound',
+    path: '/:pathMatch(.*)*',
+    component: () => import(/* webpackChunkName: "not-found" */ './views/NotFound.vue')
+  }
+];
+
+const router = createRouter({
+  // history: createWebHistory(process.env.BASE_URL),
+  // the app is deployed on GitHub Pages atm, so legacy mode is required
+  history: createWebHashHistory(process.env.BASE_URL),
+  routes
+});
+
+export default router;
