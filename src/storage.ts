3edbc1387aa26f9b40896d2141314cad8e0d2c15
julesrx <25800503+julesrx@users.noreply.github.com>
2021-07-17 20:21:32 +0200

    INDEXEDDB

--- src/storage.ts
+++ src/storage.ts
@@ -1,30 +1,11 @@
 import localforage from 'localforage';
 
 import { APP_NAME } from '~/contants';
-// import { version } from '../package.json';
-
-// const dbName = 'Glory.report';
-// const appVersionKey = 'app-version';
 
 export const getStore = (storeName: string): LocalForage => {
-  return localforage.createInstance({ name: APP_NAME, storeName });
+  return localforage.createInstance({
+    name: APP_NAME,
+    storeName,
+    driver: localforage.INDEXEDDB
+  });
 };
-
-// export const requestCacheKey = 'request-cache';
-
-// export function getStorage(storeName: string) {
-//   const store = localforage.createInstance({
-//     name: dbName,
-//     storeName
-//   });
-
-//   const localVersion = localStorage.getItem(appVersionKey);
-//   if (localVersion !== version) {
-//     store.clear();
-//     localStorage.setItem(appVersionKey, version);
-//   }
-
-//   return store;
-// }
-
-// export const requestCache = getStorage(requestCacheKey);
