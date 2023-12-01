5fbe40ed015cdbf5acc0ef85b047b375df216668
julesrx <dev@julesrx.fr>
2023-12-01 17:51:21 +0100

    pause on abort

--- composables/useDatabaseInterval.ts
+++ composables/useDatabaseInterval.ts
@@ -1,11 +1,10 @@
 export default (fn: () => void, interval = 1000) => {
     const reports = useReportStore();
     const activities = useActivitiesStore();
+    const abortController = useAbortController();
 
     const { pause } = useIntervalFn(fn, interval, { immediate: true });
 
-    whenever(
-        () => reports.totalFetched === activities.activities?.length,
-        () => pause()
-    );
+    whenever(() => reports.totalFetched === activities.activities?.length, pause);
+    useEventListener(abortController.signal, 'abort', pause);
 };
