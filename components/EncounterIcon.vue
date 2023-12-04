4292545458f3bd3409c2477ccecea1b2639e96b7
julesrx <dev@julesrx.fr>
2023-12-04 12:48:23 +0100

    url load

--- components/EncounterIcon.vue
+++ components/EncounterIcon.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import fallbackIcon from '~/assets/bungie-icon.png';
+import fallbackIcon from '~/assets/bungie-icon.png?url';
 
 const cache = useCache();
 const db = useDatabase();
