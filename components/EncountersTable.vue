c7bebbaa562286bf32dba422730a8b3b7fbee05e
julesrx <dev@julesrx.fr>
2023-12-04 13:20:55 +0100

    encounter table

---
+++ components/EncountersTable.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+defineProps<{ encounters: EncounterAggregateResult[] | null; search: string }>();
+const emit = defineEmits<{ (e: 'update:search', v: string): void }>();
+
+const onInput = (e: Event) => {
+    emit('update:search', (e.target as HTMLInputElement).value);
+};
+</script>
+
+<template>
+    <table class="w-full table-fixed">
+        <thead>
+            <tr>
+                <th class="table-cell table-left">#</th>
+                <th class="table-cell py-0 font-normal">
+                    <input
+                        id="search"
+                        :value="search"
+                        class="block bg-transparent h-full w-full focus:outline-none placeholder-stone-600"
+                        type="search"
+                        placeholder="Search..."
+                        @input="onInput"
+                    />
+                </th>
+                <th class="table-cell table-right">Matches</th>
+            </tr>
+        </thead>
+
+        <tbody v-if="encounters">
+            <EncounterListItem
+                v-for="(encounter, i) in encounters"
+                :key="encounter.membershipTypeId"
+                :encounter="encounter"
+                :index="i"
+            />
+        </tbody>
+    </table>
+</template>
--- pages/[membershipType]/[membershipId]/index.vue
+++ pages/[membershipType]/[membershipId]/index.vue
@@ -11,31 +11,6 @@ watchDebounced(search, () => refresh(), { debounce: 250 });
 
 <template>
     <div>
-        <table class="w-full table-fixed">
-            <thead>
-                <tr>
-                    <th class="table-cell table-left">#</th>
-                    <th class="table-cell py-0 font-normal">
-                        <input
-                            id="search"
-                            v-model="search"
-                            class="block bg-transparent h-full w-full focus:outline-none placeholder-stone-600"
-                            type="search"
-                            placeholder="Search..."
-                        />
-                    </th>
-                    <th class="table-cell table-right">Matches</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <EncounterListItem
-                    v-for="(encounter, i) in data"
-                    :key="encounter.membershipTypeId"
-                    :encounter="encounter"
-                    :index="i"
-                />
-            </tbody>
-        </table>
+        <EncountersTable v-model:search="search" :encounters="data" />
     </div>
 </template>
